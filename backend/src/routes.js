d80f0b1e9eedeceecf5a3593539c6fdc0d2b8c2d
marciodias <marciodias@marciodiass-iMac.local>
2020-03-25 19:30:45 -0300

    Fix

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,8 +9,10 @@ const routes = express.Router();
 
 routes.post('/session', SessionController.create);
 
-routes.get('/ongs', OngController.index);
+routes.get('/ongs', OngController.index); 
 routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index); 
 
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
