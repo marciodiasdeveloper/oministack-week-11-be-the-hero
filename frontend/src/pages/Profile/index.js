027693619c0abeebafae36696fc824b3fda1b39e
marciodias <marciodias@marciodiass-iMac.local>
2020-03-25 19:10:16 -0300

    Fix

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import './styles.css';
 
 export default function Logon() {
+  const ongId = localStorage.getItem('ongId');
+  const ongName = localStorage.getItem('ongName');
+
+  const [incidents, setIncidents] = useState([]);
+
+  useEffect(() => {
+    api.get('profile', {
+      headers: {
+        Authorization: localStorage.getItem('ongId')
+      }
+    }).then(response => {
+      setIncidents(response.data);
+    })
+  }, []);
+
   return (
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="Be The Hero" />
-        <span>Bem vinda, APAD</span>
+        <span>Bem vinda, { ongName }</span>
 
         <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
         <button type="button">
