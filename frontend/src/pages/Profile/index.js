e02a5b27c51486a9eedcd6a91f5ae832d8b3341b
marciodias <marciodias@marciodiass-iMac.local>
2020-03-25 19:14:43 -0300

    Fix

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -48,7 +48,7 @@ export default function Logon() {
             <p>{incident.description}</p>
 
             <strong>VALOR:</strong>
-            <p>{incident.value}</p>
+            <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL'}).format(incident.value)}</p>
 
             <button type="submit">
               <FiTrash2 size={20} color="#a8a8b3" />
