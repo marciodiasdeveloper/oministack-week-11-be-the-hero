b9749fa025f9ad8e2ffd9ec01906e1452dafc726
marciodias <marciodias@marciodiass-iMac.local>
2020-03-29 13:59:58 -0300

    Fix

--- backend/tests/integration/ong.spec.js
+++ backend/tests/integration/ong.spec.js
@@ -5,6 +5,7 @@ const connection = require('../../src/database/connection');
 describe('ONG', () => {
 
   beforeEach(async () => {
+    await connection.migrate.rollback();
     await connection.migrate.latest();
   }); 
 
@@ -15,6 +16,7 @@ describe('ONG', () => {
   it('should be able to create a new ONG', async () => {
     const response = await request(app)
       .post('/ongs')
+      // .set('Authorization', '134da3fc')
       .send({
         name: "Márcio Dias",
         email: "eu@marciodias.me",
